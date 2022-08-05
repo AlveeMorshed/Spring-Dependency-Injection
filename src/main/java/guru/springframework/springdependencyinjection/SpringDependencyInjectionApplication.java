c2e944969849179d4dc0373fb6afbaa1e91fb28e
AlveeMorshed <56661935+AlveeMorshed@users.noreply.github.com>
2022-08-05 15:05:06 +0600

    Add files via upload

---
+++ src/main/java/guru/springframework/springdependencyinjection/SpringDependencyInjectionApplication.java
@@ -0,0 +1,25 @@
+package guru.springframework.springdependencyinjection;
+
+import guru.springframework.springdependencyinjection.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+
+		//The following getBean method returns the Controller object since
+		// 		the MyController class is annotated with @Controller to be treated as Bean or Spring Component
+		//         So Spring is creating the controller object FOR US. We did not create it using "new MyController()"
+		MyController myController = (MyController) context.getBean("myController");
+
+		String greet = myController.sayHello();
+		System.out.println(greet);
+
+	}
+
+}
---
+++ src/main/java/guru/springframework/springdependencyinjection/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.springdependencyinjection.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World!! :D");
+        return "Hola peeps";
+    }
+}
